095a359d9dbc33956f7c197f0a1cc3b724f7011d
codejutsu1 <codejutsu@protonmail.com>
2023-12-06 16:41:56 +0100

    develop the landing page without the image

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <div class="w-1/2 m-auto flex justify-center h-[600px]">
+        <!-- <div class="w-1/2 m-auto flex justify-center h-[600px]">
             <div class="flex flex-col w-full">
                 <h1 class="text-bold text-4xl">Welcome, Team Ace</h1>
-                <p>The site is under construction...</p>
+                <p>The site is under construction...</p> -->
                 <!-- <button @click="showDID" class="px-4 py-2 bg-purple-600">Show my DID</button>
                 <textarea class="border border-gray-500 mt-10 w-full h-full p-2" v-model="text"></textarea> -->
 
-                <div class="w-1/2 mx-automt-10">
-                    <div class="py-10">
+                <!-- <div class="w-1/2 mx-auto mt-10"> -->
+                    <!-- <div class="py-10">
                         <h1 class="pb-5">Register as a service provider</h1>
                         <NuxtLink to="register" class="bg-black text-white px-4 py-2 text-semibold rounded-md">
                             Register
@@ -16,7 +16,31 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
+        <main>
+            <section class="border">
+                <div>
+
+                </div>
+            </section>
+
+            <section class="relative">
+                <div class="absolute inset-0 bg-black opacity-70 z-30"></div>
+                <div class="w-5/6 mx-auto py-16 relative z-40">
+                    <div class="w-4/5">
+                        <h1 class="text-[58px] font-bold text-white">Spend your time in your dream places, we fly the best</h1>
+                        <div class="w-4/5">
+                            <p class="my-10 font-medium text-white">
+                                Lorem ipsum dolor sit amet consectetur. Interdum vulputate fermentum odio vitae nibh nulla ornare consequat. Bibendum vel ac egestas quis venenatis. Aliquam ac cras tristique risus eros eget. Tortor felis placerat sit lobortis. Mattis mattis ornare mattis suspendisse nunc dolor. Neque magna auctor nulla gravida. Elit scelerisque at dui et condimentum tortor eu sagittis. Mauris venenatis mauris ipsum consectetur egestas magna viverra eu donec. Nisi vulputate id feugiat nec. Velit volutpat.
+                            </p>
+                        </div>
+                        <div>
+                            <button class="p-4 border text-white">Read More</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     </div>
 </template>
 
